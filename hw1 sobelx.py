dd3d198b53678118fe835ccaa056bd6200372f48
qusilen <87521424+qusilen@users.noreply.github.com>
2023-08-16 23:35:10 +0300

    Sobel Filtresi X

---
+++ hw1 sobelx.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+#cv2 => görüntü işleme
+#numpy => çok boyutlu diziler ve matematiksel işlemler
+
+def read_pgm(filename):
+    return cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+#cv2.IMREAD_GRAYSCALE görüntüyü doğrudan gri tonlamalı okumasını sağlar
+#pgm dosyasını okur
+
+def convolution(image, kernel):
+    return cv2.filter2D(image, -1, kernel)
+#görüntü ve kernel matrisi arasında konvolüsyon işlemini gerçekleştirir
+#-1 => kanal indeksi / giriş görüntüsünde renk kanallarını korumak için
+
+def write_pgm(filename, image):
+    cv2.imwrite(filename, image)
+#sonuç görüntüsünü pgm formatında kaydeder.
+
+if __name__ == "__main__":
+    input_filename = "lena.pgm"
+    output_filename = "outputsobelx.pgm"
+
+    kernel_size = 3
+    kernel_values = np.array([
+       [-1, 0, 1],
+       [-2, 0, 2],
+       [-1, 0, 1],
+    ])
+
+    input_image = read_pgm(input_filename)
+    kernel = np.array(kernel_values, dtype=np.float32)
+    
+    convolved_image = convolution(input_image, kernel)
+
+    write_pgm(output_filename, convolved_image)
+
+    print("Basariyla kaydedildi:", output_filename)
+
